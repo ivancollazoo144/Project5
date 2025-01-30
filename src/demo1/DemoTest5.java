b67df5ff75b71dd4d19775564b860d97ae58beb7
ivancollazoo <collazoivan1412@gmail.com>
2025-01-29 21:56:55 -0400

    implemented int a and b

--- src/demo1/DemoTest5.java
+++ src/demo1/DemoTest5.java
@@ -5,5 +5,12 @@ public static void main(String[] args) {
 		
 		
 		System.out.println("Java");
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a + b;
+		
+		System.out.println(result);
 	}
 }
